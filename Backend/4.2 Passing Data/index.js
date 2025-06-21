cc745c897f041e10fd505fc6141f3c0f601979e9
tiagozheng <tiago.zheng23@gmail.com>
2025-06-21 18:29:00 +0100

    Paasing data implementation practice

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -6,9 +6,16 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const fName = req.body["fName"];
+  const lName = req.body["lName"];
+  const sumName = fName.length + lName.length;
+  res.render("index.ejs", { numberOfLetters: sumName });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
